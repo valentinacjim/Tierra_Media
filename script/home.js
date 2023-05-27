51b5f91adc370ccf08bace773bf6ebe2276453e3
valentinacataldo <100451232@alumnos.uc3m.es>
2023-05-27 21:10:17 +0200

    finish categories and start with races

--- script/home.js
+++ script/home.js
@@ -1,2 +1,20 @@
-let page = document.getElementsByClassName('page')[5];
+let inicio = document.getElementsByClassName('page')[0];
+// inicio.style.visibility = 'visible';
+let historia = document.getElementsByClassName('page')[1];
+let libros = document.getElementsByClassName('page')[2];
+let series_y_peliculas = document.getElementsByClassName('page')[3];
+let personajes = document.getElementsByClassName('page')[4];
+let lugares = document.getElementsByClassName('page')[5];
+let razas = document.getElementsByClassName('page')[6];
+
+categorias = [inicio, historia, libros, series_y_peliculas, personajes, lugares, razas];
+
+function mostrarPagina(index) {
+    for (let i = 0; i < categorias.length; i++) {
+        categorias[i].style.visibility = 'hidden';
+    }
+    categorias[index].style.visibility = 'visible';
+}
+
+let page = document.getElementsByClassName('page')[7];
 page.style.visibility = 'visible';
