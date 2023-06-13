d4d0ae18fb214d3d5ab5b8bf9644eaa4d70833aa
valentinacataldo <100451232@alumnos.uc3m.es>
2023-06-13 21:12:36 +0200

    ainur

--- script/home.js
+++ script/home.js
@@ -1,19 +1,12 @@
-let inicio = document.getElementsByClassName('page')[0];
-// inicio.style.visibility = 'visible';
-let historia = document.getElementsByClassName('page')[1];
-let libros = document.getElementsByClassName('page')[2];
-let series_y_peliculas = document.getElementsByClassName('page')[3];
-let personajes = document.getElementsByClassName('page')[4];
-let lugares = document.getElementsByClassName('page')[5];
-let razas = document.getElementsByClassName('page')[6];
 
-categorias = [inicio, historia, libros, series_y_peliculas, personajes, lugares, razas];
+paginas = document.getElementsByClassName('page');
+// console.log(paginas.length);
 
 function mostrarPagina(index) {
-    for (let i = 0; i < categorias.length; i++) {
-        categorias[i].style.visibility = 'hidden';
+    for (let i = 0; i < paginas.length; i++) {
+        paginas[i].style.visibility = 'hidden';
     }
-    categorias[index].style.visibility = 'visible';
+    paginas[index].style.visibility = 'visible';
 }
 
 let page = document.getElementsByClassName('page')[7];
